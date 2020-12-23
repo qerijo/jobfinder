858276b06232df22b2f4963dc78efe1f3c4f4a65
qerijo <qerijo@gmail.com>
2020-12-23 23:02:06 +0300

    fixed routing mistake

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-var path = require('path')
-
 var express = require('express')
+var path = require('path')
+var fs = require('fs')
+var hookrouter = require('hookrouter')
 
 var app = express()
+const PORT = process.env.PORT || 8080
 
 app.use(express.static(path.join(__dirname, 'build')))
 
-app.set('port', process.env.PORT || 8080)
+app.use('*', (req, resp) => {
+  console.log('HEY', req.baseUrl)
+  hookrouter.setPath(req.baseUrl)
+  const pathIndexHTML = path.join(process.cwd(), 'build', 'index.html')
+  resp.sendFile(pathIndexHTML)
+})
 
-var server = app.listen(app.get('port'), function () {
-  console.log('listening on port ', server.address().port)
+app.listen(PORT, () => {
+  console.log(`listening at http://localhost:${PORT}`)
 })
